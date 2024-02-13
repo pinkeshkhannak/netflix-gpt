96ad5697617ddfe85ecb1bc0a697e4afa1b4b2ef
pinkeshkhannak <pinkeshkhanna56@gmail.com>
2024-02-13 17:45:51 +0530

    signin/signup

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import Browser from "./Browser";
+import Login from "./Login";
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browser />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
